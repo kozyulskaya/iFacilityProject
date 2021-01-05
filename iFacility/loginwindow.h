fe7f07fccf35f2eae00044d13176808ac0130289
E. Kozlovskaya <elenakozlovskaa15@gmail.com>
2021-01-05 22:19:09 +0700

    Add docs for Login Form

--- iFacility/loginwindow.h
+++ iFacility/loginwindow.h
@@ -11,24 +11,52 @@
 #include "administrationpanel.h"
 #include "workerpanel.h"
 
-QT_BEGIN_NAMESPACE
+/**
+ * @brief В данной неймспейсе QTFramework хранит MOC-объект, позволяющий работать с интерфейсом
+ * 
+ */
 namespace Ui { class LoginWindow; }
-QT_END_NAMESPACE
 
+/**
+ * @brief Класс интерфейса панели авторизации
+ * 
+ */
 class LoginWindow : public QMainWindow {
 private:
     Q_OBJECT
 
+    //! Статический объект разметки интерфейса
     Ui::LoginWindow *ui;
 
+    /**
+     * @brief Функция проверки и запуска соответствующих процедур при первом запуске ИС
+     * 
+     */
     void checkForFirstRun();
+    /**
+     * @brief Функция авторизации пользователя
+     * 
+     */
     void doLogin();
 
 public:
+    /**
+     * @brief Стандартный конструктор объекта интерфейса 
+     * 
+     * @param parent [ignored]
+     */
     LoginWindow(QWidget *parent = nullptr);
+    /**
+     * @brief Стандартный деструктор объекта интерфейса
+     * 
+     */
     ~LoginWindow();
 
 public slots:
+    /**
+     * @brief Слот события валидации формы авторизации
+     * 
+     */
     void validateForm();
 };
 #endif // LOGINWINDOW_H
