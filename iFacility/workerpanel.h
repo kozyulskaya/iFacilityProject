99401eb58517b9d59a2c4936f6fdbd8e002d07bb
E. Kozlovskaya <elenakozlovskaa15@gmail.com>
2021-01-05 22:01:16 +0700

    Add docs for Worker's panel

--- iFacility/workerpanel.h
+++ iFacility/workerpanel.h
@@ -6,20 +6,45 @@
 #include "objects/user.h"
 #include "viewmodels/userprofessionviewmodel.h"
 
+/**
+ * @brief В данной неймспейсе QTFramework хранит MOC-объект, позволяющий работать с интерфейсом
+ * 
+ */
 namespace Ui { class WorkerPanel; }
 
+/**
+ * @brief Класс интерфейса панели рабочего
+ * 
+ */
 class WorkerPanel : public QMainWindow {
 private:
     Q_OBJECT
 
+    //! Статический объект разметки интерфейса
     Ui::WorkerPanel *ui;
+    //! ViewModel для отображения прфоессий рабочего
     UserProfessionViewModel *upvm;
+    //! Объект пользователя рабочего
     User *mUser;
 
 public:
+    /**
+     * @brief Стандартный конструктор объекта интерфейса 
+     * 
+     * @param parent [ignored]
+     */
     explicit WorkerPanel(QWidget *parent = nullptr);
+    /**
+     * @brief Стандартный деструктор объекта интерфейса
+     * 
+     */
     ~WorkerPanel();
 
+    /**
+     * @brief Устанавливает пользователя, который в данный момент работает с панелью
+     * 
+     * @param user указатель на объект пользователя
+     */
     void setUser(User *user);
 };
 
