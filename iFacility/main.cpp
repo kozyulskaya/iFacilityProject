4363d9bf909c0b4991e3411eb4245d0713642dbb
E. Kozlovskaya <elenakozlovskaa15@gmail.com>
2021-01-05 20:33:37 +0700

    Add doc for main.cpp

--- iFacility/main.cpp
+++ iFacility/main.cpp
@@ -3,14 +3,24 @@
 
 #include "db/database.h"
 
+/**
+ * @brief Основная точка входа в приложение
+ * 
+ * @param argc Кол-во аргументов командной строки
+ * @param argv Массив аргументов командной строки
+ * @return int Код выхода приложения
+ */
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
+    //! Считываем данные в систему
     Database::instance()->load();
 
+    //! Открываем окно авторизации
     LoginWindow w;
     w.show();
 
+    //! Подписываемся на событие закрытия приложения, для сохранения данных на диск
     QObject::connect(&a, &QApplication::aboutToQuit, []() {
         Database::instance()->save();
     });
